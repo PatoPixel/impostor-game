5f82b40df4991089585282d0cefad33583873f5e
Izan <izanito2005@gmail.com>
2025-10-28 22:22:48 +0100

    Más categorías

--- src/data/categories.js
+++ src/data/categories.js
@@ -4,87 +4,199 @@ export const categories = {
     description: 'Mezcla de todas las categorías',
     words: []
   },
-  'peliculas': {
-    name: 'Películas',
-    description: 'Títulos de películas famosas',
-    words: [
-      'Titanic', 'Avatar', 'Star Wars', 'Harry Potter', 'El Rey León',
-      'Frozen', 'Toy Story', 'Shrek', 'Cars', 'Los Increíbles',
-      'Spider-Man', 'Batman', 'Superman', 'Iron Man', 'Thor',
-      'Jurassic Park', 'Terminator', 'Matrix', 'Pulp Fiction', 'Forrest Gump'
-    ]
-  },
-  'famosos': {
-    name: 'Famosos',
-    description: 'Personas famosas y celebridades',
-    words: [
-      'Cristiano Ronaldo', 'Lionel Messi', 'LeBron James', 'Serena Williams',
-      'Taylor Swift', 'Ed Sheeran', 'Beyoncé', 'Ariana Grande',
-      'Leonardo DiCaprio', 'Brad Pitt', 'Angelina Jolie', 'Jennifer Lawrence',
-      'Elon Musk', 'Bill Gates', 'Steve Jobs', 'Oprah Winfrey',
-      'Albert Einstein', 'Marie Curie', 'Nelson Mandela', 'Martin Luther King'
-    ]
-  },
-  'comida': {
-    name: 'Comida',
-    description: 'Platos y alimentos deliciosos',
-    words: [
-      'Pizza', 'Hamburguesa', 'Sushi', 'Pasta', 'Tacos',
-      'Paella', 'Ceviche', 'Ramen', 'Curry', 'Kebab',
-      'Chocolate', 'Helado', 'Tarta', 'Donuts', 'Cupcakes',
-      'Café', 'Té', 'Cerveza', 'Vino', 'Cóctel'
-    ]
-  },
-  'animales': {
-    name: 'Animales',
-    description: 'Criaturas del reino animal',
-    words: [
-      'León', 'Tigre', 'Elefante', 'Jirafa', 'Cebra',
-      'Mono', 'Panda', 'Koala', 'Delfín', 'Ballena',
-      'Águila', 'Colibrí', 'Pingüino', 'Tucán', 'Flamenco',
-      'Gato', 'Perro', 'Hamster', 'Conejo', 'Pez'
-    ]
-  },
-  'lugares': {
-    name: 'Lugares',
-    description: 'Ciudades, países y destinos',
-    words: [
-      'París', 'Londres', 'Nueva York', 'Tokio', 'Roma',
-      'Barcelona', 'Madrid', 'Sevilla', 'Valencia', 'Bilbao',
-      'Egipto', 'Japón', 'Italia', 'Francia', 'España',
-      'Disneyland', 'Torre Eiffel', 'Coliseo', 'Sagrada Familia', 'Alhambra'
-    ]
-  },
-  'deportes': {
-    name: 'Deportes',
-    description: 'Deportes y actividades físicas',
-    words: [
-      'Fútbol', 'Baloncesto', 'Tenis', 'Golf', 'Natación',
-      'Ciclismo', 'Running', 'Boxeo', 'Karate', 'Yoga',
-      'Esquí', 'Snowboard', 'Surf', 'Escalada', 'Pilates',
-      'Crossfit', 'Fitness', 'Danza', 'Ballet', 'Hip Hop'
-    ]
-  },
-  'tecnologia': {
-    name: 'Tecnología',
-    description: 'Gadgets y avances tecnológicos',
-    words: [
-      'iPhone', 'Android', 'WhatsApp', 'Instagram', 'TikTok',
-      'YouTube', 'Netflix', 'Spotify', 'Google', 'Facebook',
-      'Ordenador', 'Tablet', 'Smartwatch', 'Auriculares', 'Cámara',
-      'Drone', 'Robot', 'Inteligencia Artificial', 'Realidad Virtual', 'Blockchain'
-    ]
-  },
-  'profesiones': {
-    name: 'Profesiones',
-    description: 'Trabajos y oficios',
-    words: [
-      'Médico', 'Enfermero', 'Profesor', 'Abogado', 'Ingeniero',
-      'Chef', 'Piloto', 'Bombero', 'Policía', 'Veterinario',
-      'Arquitecto', 'Diseñador', 'Programador', 'Periodista', 'Actor',
-      'Músico', 'Pintor', 'Escritor', 'Fotógrafo', 'Psicólogo'
-    ]
-  }
+'peliculas': {
+  name: 'Películas',
+  description: 'Títulos de películas famosas',
+  words: [
+    'Titanic', 'Avatar', 'Star Wars', 'Harry Potter', 'El Rey León',
+    'Frozen', 'Toy Story', 'Shrek', 'Cars', 'Los Increíbles',
+    'Spider-Man', 'Batman', 'Superman', 'Iron Man', 'Thor',
+    'Parque Jurásico', 'Terminator', 'Matrix', 'Pulp Fiction', 'Forrest Gump',
+    'Los Vengadores', 'Guardianes de la Galaxia', 'Doctor Strange', 'Black Panther', 'Capitán América',
+    'El Caballero Oscuro', 'Origen', 'Interestelar', 'Dune', 'Oppenheimer',
+    'Barbie', 'El Padrino', 'Uno de los Nuestros', 'El Club de la Lucha', 'Sueños de Libertad',
+    'Matrix Reloaded', 'Gladiator', 'El Lobo de Wall Street', 'Joker', 'Resacón en Las Vegas',
+    'El Show de Truman', 'Buscando a Nemo', 'Up', 'Del Revés', 'Monstruos S.A.',
+    'Coco', 'Encanto', 'Vaiana', 'Zootrópolis', 'Frozen 2'
+  ]
+},
+'famosos': {
+  name: 'Famosos',
+  description: 'Personas famosas y celebridades',
+  words: [
+    'Cristiano Ronaldo', 'Lionel Messi', 'LeBron James', 'Serena Williams', 'Taylor Swift',
+    'Ed Sheeran', 'Beyoncé', 'Ariana Grande', 'Leonardo DiCaprio', 'Brad Pitt',
+    'Angelina Jolie', 'Jennifer Lawrence', 'Elon Musk', 'Bill Gates', 'Steve Jobs',
+    'Oprah Winfrey', 'Albert Einstein', 'Marie Curie', 'Nelson Mandela', 'Martin Luther King',
+    'Tom Cruise', 'Rihanna', 'Justin Bieber', 'Drake', 'Kanye West',
+    'Lady Gaga', 'Dua Lipa', 'Selena Gomez', 'Shakira', 'Bad Bunny',
+    'The Rock', 'Zendaya', 'Timothée Chalamet', 'Emma Watson', 'Robert Downey Jr.',
+    'Chris Hemsworth', 'Chris Evans', 'Mark Zuckerberg', 'Jeff Bezos', 'Johnny Depp',
+    'Keanu Reeves', 'Margot Robbie', 'Tom Holland', 'Billie Eilish', 'Post Malone',
+    'Eminem', 'Nicki Minaj', 'Doja Cat', 'Jennifer Aniston', 'Morgan Freeman'
+  ]
+},
+'comida': {
+  name: 'Comida',
+  description: 'Platos y alimentos deliciosos',
+  words: [
+    'Pizza', 'Hamburguesa', 'Sushi', 'Pasta', 'Tacos',
+    'Paella', 'Ceviche', 'Ramen', 'Curry', 'Kebab',
+    'Chocolate', 'Helado', 'Tarta', 'Donuts', 'Cupcakes',
+    'Café', 'Té', 'Cerveza', 'Vino', 'Cóctel',
+    'Sándwich', 'Perrito caliente', 'Lasaña', 'Ensalada', 'Filete',
+    'Patatas fritas', 'Nachos', 'Burrito', 'Tortitas', 'Gofres',
+    'Sopa', 'Pollo frito', 'Tortilla', 'Croissant', 'Muffin',
+    'Tarta de manzana', 'Brownie', 'Tarta de queso', 'Batido', 'Smoothie',
+    'Empanada', 'Paella valenciana', 'Pizza margarita', 'Macarrones con queso', 'Falafel',
+    'Gyoza', 'Samosa', 'Arepa', 'Taco al pastor', 'Cuscús'
+  ]
+},
+'animales': {
+  name: 'Animales',
+  description: 'Criaturas del reino animal',
+  words: [
+    'León', 'Tigre', 'Elefante', 'Jirafa', 'Cebra',
+    'Mono', 'Panda', 'Koala', 'Delfín', 'Ballena',
+    'Águila', 'Colibrí', 'Pingüino', 'Tucán', 'Flamenco',
+    'Gato', 'Perro', 'Hámster', 'Conejo', 'Pez',
+    'Caballo', 'Lobo', 'Oso', 'Zorro', 'Ciervo',
+    'Tiburón', 'Pulpo', 'Foca', 'Cocodrilo', 'Búho',
+    'Serpiente', 'Lagarto', 'Rana', 'Mariposa', 'Abeja',
+    'Hormiga', 'Araña', 'Murciélago', 'Camello', 'Mapache',
+    'Ardilla', 'Cabra', 'Oveja', 'Vaca', 'Cerdo',
+    'Gallina', 'Gallo', 'Pato', 'Loro', 'Cisne'
+  ]
+},
+'lugares': {
+  name: 'Lugares',
+  description: 'Ciudades, países y destinos famosos',
+  words: [
+    'París', 'Londres', 'Nueva York', 'Tokio', 'Roma',
+    'Barcelona', 'Madrid', 'Sevilla', 'Valencia', 'Bilbao',
+    'Egipto', 'Japón', 'Italia', 'Francia', 'España',
+    'Disneyland', 'Torre Eiffel', 'Coliseo', 'Sagrada Familia', 'Alhambra',
+    'Berlín', 'Venecia', 'Ámsterdam', 'Dubái', 'Sídney',
+    'Los Ángeles', 'San Francisco', 'Las Vegas', 'Moscú', 'Estambul',
+    'Atenas', 'Lisboa', 'Praga', 'El Cairo', 'Bangkok',
+    'Seúl', 'Pekín', 'Shanghái', 'Río de Janeiro', 'Ciudad de México',
+    'Toronto', 'Vancouver', 'Nueva Delhi', 'Ciudad del Cabo', 'La Habana',
+    'Jerusalén', 'Budapest', 'Varsovia', 'Viena', 'Zúrich'
+  ]
+},
+'deportes': {
+  name: 'Deportes',
+  description: 'Deportes y actividades físicas',
+  words: [
+    'Fútbol', 'Baloncesto', 'Tenis', 'Golf', 'Natación',
+    'Ciclismo', 'Correr', 'Boxeo', 'Kárate', 'Yoga',
+    'Esquí', 'Snowboard', 'Surf', 'Escalada', 'Pilates',
+    'Crossfit', 'Fitness', 'Danza', 'Ballet', 'Hip Hop',
+    'Béisbol', 'Hockey', 'Críquet', 'Rugby', 'Voleibol',
+    'Tenis de mesa', 'Bádminton', 'Skateboarding', 'Parkour', 'Esgrima',
+    'Remo', 'Piragüismo', 'Tiro con arco', 'Judo', 'Taekwondo',
+    'Culturismo', 'Powerlifting', 'Buceo', 'Maratón', 'Triatlón',
+    'Sumo', 'Animación deportiva', 'Esports', 'Bolos', 'Billar',
+    'Gimnasia', 'Lucha libre', 'Balonmano', 'Softbol', 'Patinaje artístico'
+  ]
+},
+'tecnologia': {
+  name: 'Tecnología',
+  description: 'Gadgets y avances tecnológicos',
+  words: [
+    'iPhone', 'Android', 'WhatsApp', 'Instagram', 'TikTok',
+    'YouTube', 'Netflix', 'Spotify', 'Google', 'Facebook',
+    'Ordenador', 'Tablet', 'Reloj inteligente', 'Auriculares', 'Cámara',
+    'Dron', 'Robot', 'Inteligencia artificial', 'Realidad virtual', 'Blockchain',
+    'Portátil', 'Monitor', 'Teclado', 'Ratón', 'Micrófono',
+    'Impresora', 'Impresora 3D', 'Smart TV', 'PlayStation', 'Xbox',
+    'Nintendo Switch', 'Router', 'WiFi', 'Bluetooth', 'USB',
+    'Disco SSD', 'Disco duro', 'Tarjeta gráfica', 'Procesador', 'Placa base',
+    'Aplicación', 'Nube', 'Servidor', 'Base de datos', 'Algoritmo',
+    'Casa inteligente', 'ChatGPT', 'Red neuronal', 'Aprendizaje automático', 'Ordenador cuántico'
+  ]
+},
+'profesiones': {
+  name: 'Profesiones',
+  description: 'Trabajos y oficios',
+  words: [
+    'Médico', 'Enfermero', 'Profesor', 'Abogado', 'Ingeniero',
+    'Chef', 'Piloto', 'Bombero', 'Policía', 'Veterinario',
+    'Arquitecto', 'Diseñador', 'Programador', 'Periodista', 'Actor',
+    'Músico', 'Pintor', 'Escritor', 'Fotógrafo', 'Psicólogo',
+    'Mecánico', 'Electricista', 'Fontanero', 'Carpintero', 'Agricultor',
+    'Científico', 'Astronauta', 'Soldado', 'Dentista', 'Farmacéutico',
+    'Traductor', 'Camarero', 'Cajero', 'Panadero', 'Carnicero',
+    'Bibliotecario', 'Recepcionista', 'Azafata de vuelo', 'Agente inmobiliario', 'Barbero',
+    'Peluquero', 'Entrenador', 'Conductor', 'Vigilante', 'Modelo',
+    'Director', 'Productor', 'Animador', 'Desarrollador de videojuegos', 'Analista de datos'
+  ]
+},
+'partido': {
+  name: 'El partido',
+  description: 'Integrantes del partido',
+  words: [
+    'Izan', 'Alex Walker', 'Arturo Gentil', 'Chente', 'David',
+    'Hosco', 'Jaime', 'Jose', 'Marco', 'Martín',
+    'Pablo', 'Peralta', 'Ramón', 'Santi', 'Arturo Estévez',
+  ]
+},
+'videojuegos': {
+  name: 'Videojuegos populares',
+  description: 'Títulos conocidos de videojuegos',
+  words: [
+    'Minecraft', 'Fortnite', 'League of Legends', 'Valorant', 'Call of Duty',
+    'Genshin Impact', 'Apex Legends', 'Counter-Strike 2', 'Overwatch 2', 'The Sims 4',
+    'Among Us', 'Roblox', 'PUBG', 'Rocket League', 'Grand Theft Auto V',
+    'The Legend of Zelda', 'Animal Crossing', 'Red Dead Redemption 2', 'Elden Ring', 'Cyberpunk 2077',
+    'Fall Guys', 'Hades', 'Terraria', 'Stardew Valley', 'The Witcher 3',
+    'Dark Souls III', 'Sekiro', 'Resident Evil 4', 'God of War: Ragnarok', 'Horizon Forbidden West',
+    'Assassin’s Creed Valhalla', 'Dota 2', 'Diablo IV', 'Baldur’s Gate 3', 'Starfield',
+    'Palworld', 'Dead by Daylight', 'Destiny 2', 'Rainbow Six Siege', 'Subnautica',
+    'Hollow Knight', 'Cuphead', 'No Man’s Sky', 'Monster Hunter: World', 'Spider-Man 2', 'Metal Gear Solid V', 'Ghost of Tsushima'
+  ]
+},
+'clashRoyale': {
+  name: 'Cartas de Clash Royale',
+  description: 'Cartas y tropas del juego Clash Royale',
+  words: [
+    'Caballero', 'Arqueras', 'Duendes', 'Esqueletos', 'Valquiria',
+    'Mini P.E.K.K.A', 'Mosquetera', 'Bebé Dragón', 'Príncipe', 'Bruja',
+    'Gigante', 'Montapuercos', 'Ariete de Batalla', 'Mago Eléctrico', 'Megaesbirro',
+    'Espíritu de Hielo', 'Espíritus de Fuego', 'Descarga', 'Flechas', 'Rayo',
+    'Bola de Fuego', 'Cohete', 'Hielo', 'Veneno', 'Cementerio',
+    'P.E.K.K.A', 'Megacaballero', 'Chispitas', 'Leñador', 'Bandida',
+    'Minero', 'Princesa', 'Arquero Mágico', 'Fantasma Real', 'Dragón Eléctrico',
+    'Cazador', 'Pescador', 'Carro de Cañón', 'Príncipe Oscuro', 'Barril de Duendes',
+    'Ejército de Esqueletos', 'Dragón Infernal', 'Gigante Esqueleto', 'Gigante Noble', 'Ballesta',
+    'Mortero', 'Tesla', 'Gólem', 'Sabueso de Lava', 'Reina Arquera'
+  ]
+},
+
+
+'lol': {
+  name: 'Campeones de League of Legends',
+  description: 'Campeones del universo de LoL',
+  words: [
+    'Aatrox', 'Ahri', 'Akali', 'Akshan', 'Alistar', 'Ambessa', 'Amumu', 'Anivia', 'Annie', 'Aphelios',
+    'Ashe', 'Aurelion Sol', 'Aurora', 'Azir', 'Bel’Veth', 'Briar', 'Bard', 'Blitzcrank', 'Brand', 'Braum',
+    'Caitlyn', 'Camille', 'Cassiopeia', 'Cho’Gath', 'Corki', 'Darius', 'Diana', 'Dr. Mundo', 'Draven', 'Ekko',
+    'Elise', 'Evelynn', 'Ezreal', 'Fiddlesticks', 'Fiora', 'Fizz', 'Galio', 'Gangplank', 'Garen', 'Gnar',
+    'Gragas', 'Graves', 'Gwen', 'Hecarim', 'Heimerdinger', 'Illaoi', 'Irelia', 'Ivern', 'Janna', 'Jarvan IV',
+    'Jax', 'Jayce', 'Jhin', 'Jinx', 'Kai’Sa', 'Kalista', 'Karthus', 'Kassadin', 'Katarina', 'Kayle', 'Kayn',
+    'Kennen', 'Kha’Zix', 'Kindred', 'Kled', 'Kog’Maw', 'K’Sante', 'LeBlanc', 'Lee Sin', 'Leona', 'Lillia',
+    'Lissandra', 'Lucian', 'Lulu', 'Lux', 'Malphite', 'Malzahar', 'Maokai', 'Master Yi', 'Mel', 'Miss Fortune',
+    'Mordekaiser', 'Morgana', 'Nami', 'Nasus', 'Nautilus', 'Neeko', 'Nilah', 'Nocturne', 'Nunu & Willump',
+    'Olaf', 'Orianna', 'Ornn', 'Pantheon', 'Poppy', 'Pyke', 'Qiyana', 'Quinn', 'Rakan', 'Rammus', 'Rek’Sai',
+    'Renata Glasc', 'Rengar', 'Riven', 'Rumble', 'Ryze', 'Samira', 'Sejuani', 'Senna', 'Seraphine', 'Sett',
+    'Shaco', 'Shen', 'Shyvana', 'Singed', 'Sion', 'Sivir', 'Skarner', 'Sona', 'Soraka', 'Swain', 'Sylas',
+    'Syndra', 'Tahm Kench', 'Taliyah', 'Talon', 'Taric', 'Teemo', 'Thresh', 'Tristana', 'Trundle', 'Tryndamere',
+    'Twisted Fate', 'Twitch', 'Udyr', 'Urgot', 'Varus', 'Vayne', 'Veigar', 'Vel’Koz', 'Vi', 'Viktor', 'Vladimir',
+    'Volibear', 'Warwick', 'Xayah', 'Xerath', 'Xin Zhao', 'Yasuo', 'Yorick', 'Yuumi', 'Zac', 'Zed', 'Zeri',
+    'Ziggs', 'Zilean', 'Zoe', 'Zyra'
+  ]
+},
+
 };
 
 // Función para obtener todas las palabras de todas las categorías
